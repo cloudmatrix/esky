c8e0389c132dcfe7e6183d7953c36bd5d06ec4ed
Ryan Kelly <ryan@rfk.id.au>
2010-03-01 09:34:46 +1100

    add copyright header

--- esky/patch.py
+++ esky/patch.py
@@ -1,3 +1,5 @@
+#  Copyright (c) 2009, Cloud Matrix Pty. Ltd.
+#  All rights reserved; available under the terms of the BSD License.
 """
 
   esky.patch:  directory diffing and patching support for esky.
