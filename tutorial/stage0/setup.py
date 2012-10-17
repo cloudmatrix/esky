b5a7b22225d91755e318302dfa3e20b321bceec2
Thomas Schaaf <schaaf@komola.de>
2012-10-17 21:34:03 +0200

    Tutorial 0

--- tutorial/stage0/setup.py
+++ tutorial/stage0/setup.py
@@ -1,10 +1,25 @@
 
-import py2exe
-from distutils.core import setup
 
-setup(
-  name = "example-app",
-  version = "0.0",
-  console = ["example.py"]
-)
+import sys
+# for windows
+# > python setup.py py2exe
+if sys.platform == 'win32' or sys.platform == 'cygwin':
+    import py2exe
+    from distutils.core import setup
 
+    setup(
+        name = "example-app",
+        version = "0.0",
+        console = ["example.py"]
+    )
+
+# for mac
+# > python setup.py py2app
+elif sys.platform == 'darwin':
+    from setuptools import setup
+    
+    setup(
+        app=["example.py"],
+        setup_requires=["py2app"],
+        options={'py2app':{}},
+    )
