fc80cd6dbeca9ee2c65ed0a89a4c05a5bcd733f8
Ryan Kelly <ryan@rfk.id.au>
2012-12-17 09:32:07 +1100

    Make master be version 0.9.8dev

--- esky/__init__.py
+++ esky/__init__.py
@@ -138,8 +138,8 @@ from __future__ import absolute_import
 
 __ver_major__ = 0
 __ver_minor__ = 9
-__ver_patch__ = 7
-__ver_sub__ = ""
+__ver_patch__ = 8
+__ver_sub__ = "dev"
 __ver_tuple__ = (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
 __version__ = "%d.%d.%d%s" % __ver_tuple__
 
